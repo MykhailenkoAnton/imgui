a4e05687cff7cefed06539dcbfa2341df43a78a1
AntonMykhailenko <mykhailencko2594@gmail.com>
2025-10-26 19:49:49 +0100

    disable c++17 dependency

--- premake5.lua
+++ premake5.lua
@@ -23,12 +23,12 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
+		--cppdialect "C++17"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
+		--cppdialect "C++17"
 
 	filter "configurations:Debug"
 		runtime "Debug"
